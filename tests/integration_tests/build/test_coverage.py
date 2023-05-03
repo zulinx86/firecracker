230846f94f942fcde347b86e3fd16f91a96a77cf
Takahiro Itazuri <itazur@amazon.com>
2023-05-03 15:16:07 +0000

    chore: Update test coverage
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- tests/integration_tests/build/test_coverage.py
+++ tests/integration_tests/build/test_coverage.py
@@ -24,7 +24,7 @@ def is_on_skylake():
 # Checkout the cpuid crate. In the future other
 # differences may appear.
 if utils.is_io_uring_supported():
-    COVERAGE_DICT = {"Intel": 83.47, "AMD": 83.02, "ARM": 82.88}
+    COVERAGE_DICT = {"Intel": 83.47, "AMD": 83.10, "ARM": 82.88}
 else:
     COVERAGE_DICT = {"Intel": 80.68, "AMD": 80.22, "ARM": 79.83}
 
