350f0fa512c7f620671ec210ac5ce90ff23d8abf
Takahiro Itazuri <itazur@amazon.com>
2024-04-25 17:57:26 +0000

    test: Run command pipeline
    This is util pipeline generation script to run arbitrary commands.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

---
+++ .buildkite/run_command.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from common import COMMON_PARSER, BKPipeline
+
+parser = COMMON_PARSER
+parser.add_argument(
+    "--command",
+    required=True,
+    help="command to run",
+    type=str,
+)
+args = COMMON_PARSER.parse_args()
+
+pipeline = BKPipeline()
+pipeline.build_group(
+    "⌘ Run Command",
+    args.command,
+)
+print(pipeline.to_json())
