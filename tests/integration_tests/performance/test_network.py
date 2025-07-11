64b49ba90d125b16c407c9f6cbcdb576ccc411ac
Takahiro Itazuri <itazur@amazon.com>
2025-07-11 10:10:22 +0000

    tmp: ping via eth1
    this is a temporary change.
    
    Signed-off-by: Takahiro Itazuri <itazur@amazon.com>

--- tests/integration_tests/performance/test_network.py
+++ tests/integration_tests/performance/test_network.py
@@ -3,7 +3,11 @@
 """Tests the network latency of a Firecracker guest."""
 
 import json
+import os
 import re
+import signal
+import subprocess
+import time
 from pathlib import Path
 
 import pytest
@@ -49,6 +53,7 @@ def network_microvm(request, microvm_factory, guest_kernel_acpi, rootfs):
     vm.spawn(log_level="Info", emit_metrics=True)
     vm.basic_config(vcpu_count=guest_vcpus, mem_size_mib=guest_mem_mib)
     vm.add_net_iface()
+    vm.add_net_iface()
     vm.start()
     vm.pin_threads(0)
 
@@ -74,13 +79,35 @@ def test_network_latency(network_microvm, metrics):
     )
 
     samples = []
-    host_ip = network_microvm.iface["eth0"]["iface"].host_ip
+    host_ip = network_microvm.iface["eth1"]["iface"].host_ip
+    ssh = network_microvm.ssh
+    tcpdump_result = "/tmp/capture.pcap"
 
-    for _ in range(rounds):
-        _, ping_output, _ = network_microvm.ssh.check_output(
-            f"ping -c {request_per_round} -i {delay} {host_ip}"
+    for round in range(rounds):
+        cmd = [
+            "ip",
+            "netns",
+            "exec",
+            ssh.netns,
+            "ssh",
+            "-tt",
+            *ssh.options,
+            ssh.user_host,
+            f"tcpdump -i eth1 -U -w {tcpdump_result}",
+        ]
+        tcpdump = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, start_new_session=True
         )
-
+        time.sleep(1)  # give tcpdump some time to start up
+        _, ping_output, _ = ssh.check_output(
+            f"ping -I eth1 -c {request_per_round} -i {delay} {host_ip}"
+        )
+        time.sleep(1)
+        os.killpg(tcpdump.pid, signal.SIGINT)
+        _, dmesg_output, _ = ssh.check_output("dmesg")
+        print(dmesg_output)
+        _, tcpdump_output, _ = ssh.check_output(f"tcpdump -r {tcpdump_result}")
+        print(f"round #{round}\n{tcpdump_output}")
         samples.extend(consume_ping_output(ping_output, request_per_round))
 
     for sample in samples:
